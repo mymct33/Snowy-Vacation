0b78a8385cfd1a4f2473441ed0c4b53530eae6a1
Connor S <mymct33@gmail.com>
2020-11-23 09:37:17 +1100

    Began Radio Work

---
+++ Snowy-Vacation-Under-Template/Assets/Tasty Gamers/Scripts/Radio/RadioSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSwitch : MonoBehaviour
+{
+    public List<AudioClip> Songs;
+    private AudioSource sPlayer;
+    public int CurrentSong;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            Transform tChild = gameObject.transform.GetChild(i);
+            if(tChild.GetComponent<AudioSource>())
+            {
+                sPlayer = tChild.GetComponent<AudioSource>();
+                sPlayer.clip = Songs[0];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sPlayer.Play();
+    }
+
+    private void ToggleUp()
+    {
+        
+    }
+}
