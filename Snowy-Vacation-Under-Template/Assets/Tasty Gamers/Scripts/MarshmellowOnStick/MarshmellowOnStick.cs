bda933d0b1d40c7b26fb7a6c40bd705a0cedd082
Connor S <mymct33@gmail.com>
2020-11-30 15:32:25 +1100

    start of the marshmellows

---
+++ Snowy-Vacation-Under-Template/Assets/Tasty Gamers/Scripts/MarshmellowOnStick/MarshmellowOnStick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarshmellowOnStick : MonoBehaviour
+{
+    public GameObject VRAvatar;
+    private  GameObject Hand;
+    public void AttachToController()
+    {
+        for(int i = 0; i < VRAvatar.transform.childCount; i++)
+        {
+            Transform tChild = VRAvatar.transform.GetChild(i);
+            if(tChild.name == "PrimaryHand")
+            {
+                Hand = tChild.gameObject;
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Hand.transform.position, 1);
+            }
+        }
+    }
+}
